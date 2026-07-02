39bd2d2aaa9b9a6d1030cd05c7fef815ca2c12c0
CivicPeek <tiusean061903@gmail.com>
2026-07-01 19:27:47 -0700

    Update login UI

--- app/src/main/java/layer8project/App.java
+++ app/src/main/java/layer8project/App.java
@@ -1,6 +1,8 @@
 package layer8project;
+
 import javax.swing.*;
 import java.awt.*;
+
 //"import javax.swing.*;" is a prebuilt GUI tool that helps you create windows, buttons, and text boxes
 //^ In short it helps with quality of life 
 //import java.awt.*; is short for Abstract Window Tool and by adding this it allows the user to have a more basic framework of the GUI 
@@ -21,65 +23,93 @@ public class App {
         //^"frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);"
         // This allows the user to close the app
 
-        JLabel username = new JLabel("Username");
-        JTextField usernamefield = new JTextField(15);
-        //Creation of a box where the User can input their Username
-
-        JLabel password = new JLabel("Password");
-        JPasswordField passwordfield = new JPasswordField(15);
-        //Creation of a box where the User can put the password for their account
-
-        JButton button1 = new JButton("Forget Password");
-        //Creation of a Forget Password button
-
-        JButton button = new JButton("Login");
-        //Creation of a Login button
-
-        ImageIcon bg = new ImageIcon(App.class.getResource("/Images/Logo.png"));
-        JLabel background = new JLabel(bg);
-
-
-        background.setBounds(1,1,20, 20);
-        frame.add(background);
-
+        frame.getContentPane().setBackground(new Color(30,30,30));
         frame.setLayout(new GridBagLayout());
+        // Creation of the background color along with the layout inside the frame
+
         JPanel panel = new JPanel();
-        //A panel helps us add add objects without them overlapping through the use of JFrame
-        panel.setLayout(new GridLayout( 3, 3, 1, 1));
-        // The GridLayout helps create a grid inside out app which allows us to center the objects we created.
+        panel.setBackground(new Color(30,30,30));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        //The creation of the panel allows us to edit anything inside the frame
+        // and the setLayout allows us to stack them vertically  
 
-        
+        ImageIcon logo = new ImageIcon(App.class.getResource("/Images/Logo.png"));
+        Image scaled = logo.getImage().getScaledInstance(250,120,Image.SCALE_SMOOTH);
+        // This loads the image saved under Logo.png and resizes it so its 250x120
 
-        //background Color
-        panel.setBackground(Color.WHITE);
-        panel.setBackground(new Color(30, 30 , 30));
 
-        // This allows us to set the background of the entire window to dark
-        frame.getContentPane().setBackground(new Color (30, 30 ,30));
+        JLabel background = new JLabel(new ImageIcon(scaled));
+        background.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        //Button colors and button background colors
-        button.setBackground(new Color (0,120,255));
-        button.setForeground(Color.BLACK);
+        panel.add(background);
+        panel.add(Box.createVerticalStrut(10));
+        //pane.add(background) puts the logo inside the panel that we added above 
+        //createVerticalStrut creates a set amount of pixels of empty spaces
 
-        button1.setBackground(new Color (0,120,255));
-        button1.setForeground(Color.BLACK);
-
+        JLabel username = new JLabel("Username");
         username.setForeground(Color.WHITE);
-        password.setForeground(Color.WHITE);
+        username.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //Creation of the username
+        //setForeground makes the text white 
+        //setAlignment centers the word "Username"
+
+        JTextField usernamefield = new JTextField();
+        usernamefield.setMaximumSize(new Dimension(300,40));
+        // This creates the space where the user types
+        //setMaximumSize creates the size of the textbox
 
         panel.add(username);
+        panel.add(Box.createVerticalStrut(5));
         panel.add(usernamefield);
-        
-        panel.add(password);
-        panel.add(passwordfield);
+        panel.add(Box.createVerticalStrut(20));
+        // This adds the username and password into the panel
 
-        panel.add(button);
-        panel.add(button1);
+        JLabel password = new JLabel("Password");
+        password.setForeground(Color.WHITE);
+        password.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // The password label is created 
+        //setForeground makes the text white and setAlignment centers it
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setMaximumSize(new Dimension(300,40));
+        //Creates the password box and rathern than showing the letters it comes out as *
+
+        panel.add(password);
+        panel.add(Box.createVerticalStrut(5));
+        panel.add(passwordField);
+        panel.add(Box.createVerticalStrut(10));
+        // Same thing with the usernamefield, it adds on them on the panel
+
+        JButton forgot = new JButton("Forgot Password");
+        forgot.setBorderPainted(false);
+        forgot.setContentAreaFilled(false);
+        forgot.setForeground(new Color(0,120,255));
+        forgot.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //setBorderPainted(false); makes it so that the border around the forgot button is hidden
+        //setContentAreaFilled(false); removes the background of the button
+
+        panel.add(forgot);
+        panel.add(Box.createVerticalStrut(30));
+        //Creation of a box where the User can put the password for their account
+
+        JButton login = new JButton("LOGIN");
+        login.setBackground(new Color(0,120,255));
+        login.setForeground(Color.BLACK);
+        login.setFocusPainted(false);
+        login.setMaximumSize(new Dimension(300,45));
+        login.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // Creation of the LOGIN button
+        // Along with the coloring, color of the text, background and restricts the sizing of the button
+
+        panel.add(login);
+        //Creation of a Login button
 
         frame.add(panel);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        //This adds the objects we made into a panel and by setting it visible we are able to see them.
-        // "frame.setVisible(b:true)" allows the window itself to open.
-        //If it was false then the window would be hidden and not reveal itself
+        //frame.add(panel) adds the panel into the frame such as Logo, Username, Textbox etc
+        //setLocationRelativeTo(null) centers the users screen
+        //Makes the window visible
+
     }
     }
